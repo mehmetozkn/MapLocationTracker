d96e12ee2d0c609b6184fb9c60e28120055cef3d
Mehmet Özkan <mehmet.trozkan@gmail.com>
2025-05-21 22:42:06 +0300

    refactor

---
+++ MapLocationTracker/Models/UserLocationModel.swift
@@ -0,0 +1,11 @@
+//
+//  UserLocationModel.swift
+//  MapLocationTracker
+//
+//  Created by Mehmet Özkan on 20.05.2025.
+//
+
+struct UserLocation: Equatable {
+    let latitude: Double
+    let longitude: Double
+}
